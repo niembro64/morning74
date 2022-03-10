0fc85786327720ec0a83156b0a512c44b43353c5
=Eric Niemeyer <=niemeyer.eric@gmail.com>
2022-03-10 11:50:56 -0500

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+// functional
+
+import logo from "./logo.svg";
+import "./App.css";
+import "bootstrap/dist/cwss/bootstrap.min.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>
+                <marquee behavior="" direction="">
+                    test
+                </marquee>
+            </h1>
+            
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/ClassCounter.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+
+class ClassCounter extends Component{
+    constructor(props){
+        super(props);
+
+    }
+
+
+    render (){
+        return (
+
+
+
+            
+        )
+    }
+
+
+}
