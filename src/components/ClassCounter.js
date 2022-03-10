ab0259aac1db370d3a6af73165eb0531aed03fcc
=Eric Niemeyer <=niemeyer.eric@gmail.com>
2022-03-10 12:18:58 -0500

    class vs functional count

--- src/components/ClassCounter.js
+++ src/components/ClassCounter.js
@@ -3,14 +3,25 @@ import React, { Component } from "react";
 class ClassCounter extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            counter: 0
+        }
+    }
+
+    increaseCounter = () => {
+        // console.log("increase");
+
+        this.setState ({
+            counter: this.state.counter + 1
+        })
     }
 
     render() {
         const {text} = this.props
         return (
             <div className="border border-dark w-25">
-                <h1>count</h1>
-                <button className="btn btn-primary btn-lg">click me!</button>
+                <h1>Count: {this.state.counter}</h1>
+                <button onClick={this.increaseCounter} className="btn btn-primary btn-lg">click me!</button>
                 <p>
                     <em>this is class component</em>
                 </p>
--- src/components/FunctionalCounter.js
+++ src/components/FunctionalCounter.js
@@ -1,12 +1,18 @@
-import React from "react";
+import React, {useState} from "react";
 
 const FunctionCounter = (props) => {
-    const {text} = props
+    const {text} = props;
+    const [state, setState] = useState(0);
+
+    const increaseCounter = () => {
+        console.log("increase functional");
+        setState(state+1);
+    }
 
     return (
         <div className="border border-dark w-25">
-            <h1>count</h1>
-            <button className="btn btn-primary btn-lg">click me!</button>
+            <h1>Count: {state}</h1>
+            <button onClick={increaseCounter} className="btn btn-primary btn-lg">click me!</button>
             <p>
                 <em>this is functional component</em>
             </p>
